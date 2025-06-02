cae12b33f89efe38020b0705d9d9c820255de368
Danil <Danil@DESKTOP-99KGHPD>
2025-06-02 17:24:27 +1000

    Add project files.

---
+++ ChessBoard/ChessBoard.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int size;
+	cout << "Введите размер шахматной доски: "; cin >> size;
+	bool counter = 1;
+	for (int i = 0; i < size; i++)
+	{
+		for (int b = 0; b < size; b++)
+		{
+			counter == 1 ? cout << "+ " : cout << "- ";
+			counter == 1 ? counter = 0 : counter = 1;
+		}
+		cout << endl;
+		if (size % 2 == 0 and i%2==0)
+		{
+			counter = 0;
+		}
+		else if (size % 2 == 0)
+		{
+			counter = 1;
+		}
+	}
+}
